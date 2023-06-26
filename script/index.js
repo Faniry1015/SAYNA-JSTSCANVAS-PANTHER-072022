5e1a8cf86470bed47e19283babf2a0fb0743dbbb
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-06-26 16:59:57 +0300

    Index Message pupup box Done

--- script/index.js
+++ script/index.js
@@ -1,8 +1,36 @@
-let submitBtnForm = document.getElementById("submitBtnForm")
+//Message validator popup Box
+const myForm = document.querySelector(".sec4FormSec__form")
+const userName = document.querySelector("#userName")
+const noName = document.querySelector(".noName")
+const userMail = document.querySelector("#userMail")
+const invalidMail = document.querySelector(".invalidMail")
+const mainMessage = document.querySelector("#mainMessage")
+const warningMessage = document.querySelector(".warningMessage")
+const messagePopupBox = document.querySelector(".messagePopupBox")
 
-let popupBox = document.getElementById("popupBox")
-
-submitBtnForm.addEventListener("click", () => {
-        popupBox.style.display = "block"
-    })
+myForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    if (userName.value === "") {
+        noName.innerText = "Champ obligatoire: Entrez votre Nom !"
+        noName.style.color = "red"
+        return
+    }else if (userMail.value === "") {
+        noName.innerText = ""
+        warningMessage.innerText = ""
+        invalidMail.innerText = "Champ obligatoire: Entrez votre addresse mail !"
+        invalidMail.style.color = "red"
+        return
+    } else if (mainMessage.value === "") {
+        noName.innerText = ""
+        invalidMail.innerText = ""
+        warningMessage.innerText = "Champ obligatoire: Entrez votre message !"
+        warningMessage.style.color = "red"
+        return
+    } else {
+        noName.innerText = ""
+        invalidMail.innerText = ""
+        warningMessage.innerText = ""
+    }
+    messagePopupBox.style.display = "flex"
+})
 
