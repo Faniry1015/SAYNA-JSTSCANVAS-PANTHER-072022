22b8f2b0a6c1eb8fe60543b3a27fa52fe0d4293b
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-06-30 17:31:59 +0300

    Scale title h1 and h2 mouseover with JS

--- script/enigScript.js
+++ script/enigScript.js
@@ -1,3 +1,5 @@
+/**Quizz */
+/**Selecteurs */
 const quizzAnswerForm = document.querySelector(".quizzAnswerForm")
 const submitAnswer = document.querySelector("#EnigSubmitBtnForm")
 const resultPopupBox = document.querySelector("#EnigPopupBox__mainContainer")
@@ -20,14 +22,14 @@ const doYouKnow = document.querySelector(".doYouKnow")
 
 const indexPage = document.querySelector(".indexPage")
 
+/**Submit answer */
 quizzAnswerForm.addEventListener("submit", (e) => {
     e.preventDefault()
     resultPopupBox.style.display = "flex"
-    const userAnswerVal = userAnswer.value.toUpperCase()
-    console.log(userAnswerVal)
-    if(userAnswerVal === enigmeAnswer) {
+    const userAnswerVal = userAnswer.value.toUpperCase().replaceAll(" ", "")
+    console.log(userAnswerVal, enigmeAnswer.toUpperCase().replaceAll(" ", ""))
+    if(userAnswerVal === enigmeAnswer.toUpperCase().replaceAll(" ", "")) {
         if(qIndex >= qTab.length) {
-            console.log("congrat")
             congrat.innerText = "Ton initiation est terminée !"
             resultComment.innerText = ""
             nextQ.style.display = "none"
@@ -44,6 +46,8 @@ quizzAnswerForm.addEventListener("submit", (e) => {
     }
 })
 
+
+/**Next question si reponse est vraie */
 let qIndex = 0
 let qNumber = 1
 nextQ.addEventListener("click", (e) => {
@@ -60,6 +64,7 @@ nextQ.addEventListener("click", (e) => {
     ++qIndex
 })
 
+/**Fermer quizz popupbox */
 document.addEventListener("click",() => {
     resultPopupBox.style.display = "none"
 } )
@@ -70,18 +75,18 @@ enigGoIndex.addEventListener("click",() => {
     indexPage.click()
 } )
 
-//Tableau des résultats du quizz
-const qTab = [
+//Tableau des questions 2 et 3 du quizz
+const qTab = [ 
     {
         question: "Une anecdote intéressante est écrite juste en dessous mais elle est codée par un code césar également appelé code de chiffrement par décalage . L’alphabet a été décalé, trouve la clé de chiffrement qui te permettrait de retrouver les lettres et retranscris la phrase :",
         indice: "Tm nqtu lmjcbm i Wikstivl mv Kitqnwzvqm. Qt a'ioqb lm ti dqttm lwvb mab wzqoqviqzm Zgiv Kwwotmz mb moitmumvb ti dqttm ycq i dc viqbzm tm uwcdmumvb xwtqbqycm lma jtiks xivbpmza",
-        reponse: "",
+        reponse: "Le film debute a Oackland en Californie. Il s'agit de la ville dont est originaire Ryan Coogler et egalement la ville qui a vu naitre le mouvement politique des black panthers",
         doYouKnow:"Plusieurs sources s’entendent pour dire que la lettre la plus utilisée en français est la lettre E. En pourcentage de fréquence, la lettre est utilisée à 14% dans une phrase. Et si tu regardais quel symbole revient le plus souvent pour en déduire son décalage dans l’alphabet ? ",
     },
     {
         question: "Lorsque T’Challa mange l’herbe en forme de coeur pour recevoir les pouvoirs du Black Panther, il voit son père afin de lui demander conseil afin de devenir un bon roi. Cette scène rappelle la même scène d’un certain film où un père dit à son fils de ne pas oublier qui il est et d’où il vient. Quel est ce film ?",
         indice: "01001100 01000101 00100000 01010010 01001111 0100100<br>00100000 01001100 01001001 01001111 01001110 00001101<br>00001010",
-        reponse: "",
+        reponse: "Le roi lion",
         doYouKnow: "Le philosophe Francis Bacon inventa en 1605 un alphabet bilitère, uniquement composé des deux lettres A et B. Cest en quelque sorte lancêtre du système binaire des ordinateurs actuels car toute lettre pouvait être construite avec un enchainement précis de ces deux lettres, tandis que le système binaire informatique utilise 0 et 1."
     }
 ]
--- script/main.js
+++ script/main.js
@@ -16,7 +16,7 @@ const handleIntersect = function (entries, observer) {
 }
 
 const observer = new IntersectionObserver(handleIntersect, options)
-document.querySelectorAll(".reveal").forEach(function(r){
+document.querySelectorAll(".reveal").forEach(function (r) {
     observer.observe(r)
 })
 
@@ -25,6 +25,20 @@ const socialLeftDiv = document.querySelector(".reseauxSoc")
 socialLeftDiv.animate([
     { opacity: 0, transform: 'translateY(-300px)' },
     { opacity: 1, transform: 'translateY(0)' }
-  ], {
+], {
     duration: 1500,
-  });
+});
+
+//Zoom sur les titres H1 et H2
+let titleHtoScale = document.querySelectorAll("h2, h1")
+titleHtoScale.forEach(title => {
+    title.addEventListener("mouseover", () => {
+        title.style.transform = "scale(1.1)"
+    })
+})
+titleHtoScale.forEach(title => {
+    title.addEventListener("mouseout", () => {
+        title.style.transform = "none"
+    })
+})
+
