9762f3572423db6f9b71252700ab31dfc653d67e
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-06-30 05:27:25 +0300

    enigme popupbox css nearly done

--- script/enigScript.js
+++ script/enigScript.js
@@ -5,8 +5,20 @@ const userAnswer = document.querySelector("#reponse")
 
 quizzAnswerForm.addEventListener("submit", (e) => {
     e.preventDefault()
-    resultPopupBox.style.display = "block"
+    resultPopupBox.style.display = "flex"
     const userAnswerVal = userAnswer.value.toLowerCase()
     console.log(userAnswerVal)
 
 })
+
+//Tableau des résultats du quizz
+const quizzResultTab = [
+    {
+        result: "Bravo !",
+        comment: "Tu as trouvé la réponse. Es tu prêt pour l'énigme suivante ?"
+    },
+    {
+        result: "Bravo !",
+        comment: "Tu as trouvé la réponse. Es tu prêt pour l'énigme suivante ?"
+    }
+]
